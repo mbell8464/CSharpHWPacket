5a8d8bfc5e861fae2c16b3d8f5f1d75d033fb127
mbell8464 <michaelb2127@charter.net>
2021-02-14 21:15:50 -0600

    Completed Lab Set "Student". Created Custom Class for "Student" then displayed data using the class to my TesterProgram

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //fields
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public float GPA
+        {
+            get { return _gpa; }
+            set
+            {
+                _gpa = value >= 0 && value <= 4 ? value : 0;
+            }
+        }
+
+        //constructors
+        //Default CTOR
+        public Student() { }
+
+        //FQ CTOR
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            GPA = gpa;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format($"First Name: {FirstName}\nLast Name: {LastName}\nStudent ID: {Id}\nGPA: {GPA}\n");
+        }
+
+    }
+}
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,6 +11,22 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            //************STUDENT*************
+            Console.WriteLine("\n\n******STUDENT*******\n\n");
+
+            //long way
+            Student s1 = new Student();
+            s1.FirstName = "Michael";
+            s1.LastName = "Scott";
+            s1.Id = "1234";
+            s1.GPA = 2.87f;
+
+            //short way
+            Student s2 = new Student("Dwight", "Schrute", "1235", 3.17f);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
         }
     }
 }
